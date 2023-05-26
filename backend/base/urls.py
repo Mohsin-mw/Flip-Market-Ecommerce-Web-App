6498bf00ff6f04b26548f5e8e3dc490ee75bd7a1
MMW <null.mmw@gmail.com>
2023-05-26 14:50:20 -0700

    JSON web token Authentication added

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+
+
+
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
     path('product/<str:pk>', views.getProduct, name="product"),
+    path('user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
