5e898aa327719511f7fd48906cee93067885c9f5
MMW <null.mmw@gmail.com>
2023-05-26 23:09:39 -0700

    Customized JSON web token

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
     path('product/<str:pk>', views.getProduct, name="product"),
-    path('user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,29 @@
+from typing import Dict, Any
+
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .products import products
 from .serializers import ProductSerializer
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, str]:
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 def getRoutes(request):
     return JsonResponse("Hello", safe=False)
