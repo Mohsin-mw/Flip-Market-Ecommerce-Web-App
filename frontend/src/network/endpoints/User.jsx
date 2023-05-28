b8e66a359ec0c8bcc050bc1bb4b8ecd4ff481fed
MMW <null.mmw@gmail.com>
2023-05-28 09:19:59 -0700

    Revert "User Reducer Added"
    This reverts commit 63002d8cce132a1a3bd7ef4b0913b5e12500abaf.

--- frontend/src/network/endpoints/User.jsx
+++
@@ -1,8 +0,0 @@
-import { axiosClient } from "../ApiClient";
-
-export function UserLogin(email, password) {
-  return axiosClient().post("/api/users/login/", {
-    username: email,
-    password: password,
-  });
-}
--- frontend/src/store/Store.jsx
+++ frontend/src/store/Store.jsx
@@ -3,7 +3,6 @@ import ProductReducer from "./Slices/Product/ProductSlice";
 import AppReducer from "./Slices/App/AppSlice";
 import SingleProduct from "./Slices/SingleProduct/SingleProductSlice";
 import CartReducer from "./Slices/Cart/CartSlice";
-import UserReducer from "./Slices/User/UserSlice";
 
 export const store = configureStore({
   reducer: {
@@ -11,6 +10,5 @@ export const store = configureStore({
     productList: ProductReducer,
     singleProduct: SingleProduct,
     cart: CartReducer,
-    user: UserReducer,
   },
 });
